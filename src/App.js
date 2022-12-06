cd178ed9eac5cb1c12f9dd93cb38c40a0e6f1ff4
amontalvof <amontalvof92@gmail.com>
2022-12-06 10:16:43 -0600

    feat: Using Props

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import StyledButton from './components/Button';
 function App() {
     return (
         <div className="App">
-            {/* <button>Button</button> */}
             <StyledButton>Styled Button</StyledButton>
+            <div>
+                <br />
+            </div>
+            <StyledButton variant="outline">Styled Button</StyledButton>
         </div>
     );
 }
